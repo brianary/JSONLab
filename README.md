2c5fda3c89b6fb3882f97d169dd8230f12a3992e
Brian Lalonde <brianary@gmail.com>
2026-05-18 21:40:44 -0700

    Initial version

---
+++ README.md
@@ -0,0 +1,15 @@
+﻿JSONLab
+=======
+
+<!-- To publish to PowerShell Gallery, commit an update to the .psd1 file -->
+<img src="images/JSONLab.svg" alt="JSONLab icon" align="right" height="200" width="200" />
+
+[![PowerShell Gallery Version](https://img.shields.io/powershellgallery/v/JSONLab)](https://www.powershellgallery.com/packages/JSONLab/)
+[![PowerShell Gallery](https://img.shields.io/powershellgallery/dt/JSONLab)](https://www.powershellgallery.com/packages/JSONLab/)
+[![Actions Status](https://github.com/brianary/JSONLab/actions/workflows/continuous.yml/badge.svg)](https://github.com/brianary/JSONLab/actions/workflows/continuous.yml)
+[![Mastodon: @dataelemental@mastodon.social](https://badgen.net/badge/@dataelemental/@mastodon.social/blue?icon=mastodon)](https://mastodon.social/@dataelemental "DataElemental Mastodon profile")
+
+A collection of general-purpose functions for objects, properties, and more.
+
+<img src="images/demo.gif" alt="a demonstration of the module commands" height="300" width="600" />
+
---
+++ src/public/Merge-Json.ps1
@@ -0,0 +1,50 @@
+﻿<#
+.SYNOPSIS
+Create a new JSON string by recursively combining the properties of JSON strings.
+
+.INPUTS
+System.String of JSON to combine.
+
+.OUTPUTS
+System.String of JSON combining the inputs.
+
+.FUNCTIONALITY
+Json
+
+.LINK
+Merge-PSObject.ps1
+
+.LINK
+ConvertFrom-Json
+
+.LINK
+ConvertTo-Json
+
+.EXAMPLE
+'{"a":1,"b":{"u":3},"c":{"v":5}}','{"a":{"w":8},"b":2,"c":{"x":6}}' |Merge-Json.ps1
+
+{
+    "a":  {
+            "w":  8
+        },
+    "b":  2,
+    "c":  {
+            "v":  5,
+            "x":  6
+        }
+}
+#>
+
+#Requires -Version 3
+[CmdletBinding()][OutputType([string])] Param(
+<#
+JSON string to combine. Descendant properties are recursively merged.
+Primitive values are overwritten by any matching ones in the new JSON string.
+#>
+[Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true,ValueFromRemainingArguments=$true)][string[]] $InputObject,
+# Omits white space and indented formatting in the output string.
+[switch]$Compress
+)
+Begin {$value = [pscustomobject]@{}}
+Process {$value = $value,($InputObject |ConvertFrom-Json) |Merge-PSObject.ps1}
+End {$value  |ConvertTo-Json -Compress:$Compress}
